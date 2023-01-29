20673ae20aad138053cb6a73e58bf7895d320934
Tennis Kumar C <70009826+tennis-kumar@users.noreply.github.com>
2023-01-29 16:15:05 +0530

    Level - 3 Hashing Passwords

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 
 mongoose.set("strictQuery", true);
@@ -29,10 +28,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // console.log(process.env.SECRET);
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -55,7 +54,7 @@ app.post( "/register", function(req, res){
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save( function(err){
@@ -70,7 +69,7 @@ app.post( "/register", function(req, res){
 
 app.post( "/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne( {email: username}, function(err, result){
         if(err){
